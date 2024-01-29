b510f3781328632f4d7c42fcf33426873f874d1b
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-01-29 21:13:30 +0530

    1.3

---
+++ Assets/My_Assets/Scripts/Enemies/New_Zombai.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New_Zombai : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+    private float distance;
+
+
+    private void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        
+    }
+}
--- Assets/My_Assets/Scripts/Healths.cs
+++ Assets/My_Assets/Scripts/Healths.cs
@@ -6,13 +6,10 @@ public class Healths : MonoBehaviour, IDamageable
 {
     public UnityEvent OnDeath, OnDamage;
     [field: SerializeField, Min(0)] public int max { set; private get; } = 100;
-
     [SerializeField, ReadOnly] private int value;
-
     [SerializeField, Range(0f, 5f)] private int damageMultipler = 1;
 
     public int Value() => value;
-
     private void Start()
     {
         value = max;
@@ -21,7 +18,6 @@ public class Healths : MonoBehaviour, IDamageable
     void IDamageable.OnDamage(int amount)
     {
         value = Mathf.Clamp(value - amount * damageMultipler, 0, max);
-
         if (value is 0) OnDeath.Invoke();
         else OnDamage.Invoke();
     }
