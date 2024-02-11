93d3f59e17cbeb49b8976650a8999e443576a8a5
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-02-11 21:11:30 +0530

    gun

--- Assets/My_Assets/Scripts/Bullet.cs
+++ Assets/My_Assets/Scripts/Bullet.cs
@@ -1,32 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private Rigidbody2D rb;
+      [SerializeField] private TrailRenderer trail;
+      [SerializeField] private Rigidbody2D body;
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-    private void Start()
-    {
-        gameObject.SetActive(false);
-    }
+      private void Reset()
+      {
+            trail = GetComponent<TrailRenderer>();
+            body = GetComponent<Rigidbody2D>();
+      }
+      private void Start()
+      {
+            SetActive(false);
+      }
+      public void Fire(Vector2 Direction, float force)
+      {
+            SetActive(true);
+            body.AddForce(Direction * force, ForceMode2D.Impulse);
+      }
+      private void OnCollisionEnter2D(Collision2D collision)
+      {
+            if (collision.transform.TryGetComponent(out IDamageable ID))
+            {
+                  ID.OnDamage(4);
+            }
+            SetActive(false);
+      }
+      private void OnBecameInvisible()
+      {
+            SetActive(false);
+      }
 
-    public void Fire( Vector2 Direction, float force)
-    {
-        rb.AddForce(Direction * force, ForceMode2D.Impulse);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.transform.TryGetComponent(out IDamageable ID))
-        {
-            ID.OnDamage(4);
-        }
-        Destroy(gameObject);
-    }
-
+      private void SetActive(bool value)
+      {
+            trail.Clear();
+            gameObject.SetActive(value);
+      }
 }
--- Assets/My_Assets/Scripts/Gun.cs
+++ Assets/My_Assets/Scripts/Gun.cs
@@ -4,43 +4,44 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
 
-    public ushort PistolAmmo, RifleAmmo;
-   
-    [SerializeField] private Bullet bullet;
-    [SerializeField] private Transform nozzle;
+      public ushort PistolAmmo, RifleAmmo;
 
-   [SerializeField] private List<Bullet> bullets;
+      [SerializeField] private Bullet bullet;
+      [SerializeField] private Transform nozzle;
+
+      [SerializeField] private List<Bullet> bullets;
 
 
-    private void Start()
-    {
-        for(int i=0; i< 5; i++)
-        {
-            bullets.Add(Instantiate(bullet,nozzle.position,Quaternion.identity));
-        }
-    }
-    public void Shoot(int ID)
-    {
-        switch(ID)
-        {
-            case 0: // Pistol
-                {
-                    foreach(var bullet in bullets)
-                    {
-                        if (bullet.gameObject.activeSelf) continue;
-                        bullet.transform.position =nozzle.position;
-                        bullet.gameObject.SetActive(true);
-                        bullet.Fire(-nozzle.up, 100f);
+      private void Start()
+      {
+            for (int i = 0; i < 5; i++)
+            {
+                  bullets.Add(Instantiate(bullet, nozzle.position, Quaternion.identity));
+            }
+      }
+      public void Shoot(int ID)
+      {
+            switch (ID)
+            {
+                  case 0: // Pistol
+                        {
+                              foreach (var bullet in bullets)
+                              {
+                                    if (bullet.gameObject.activeSelf) continue;
+                                    bullet.transform.position = nozzle.position;
+                                    bullet.gameObject.SetActive(true);
+                                    bullet.Fire(-nozzle.up, 100f);
+                                    break;
+                              }
+                        }
                         break;
-                    }
-                }
-                break;
 
-            case 1: // Rifle
-                {
+                  case 1: // Rifle
+                        {
 
-                }break;
-        }
-    }
-    
+                        }
+                        break;
+            }
+      }
+
 }
