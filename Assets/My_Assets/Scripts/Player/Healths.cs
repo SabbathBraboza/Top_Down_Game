2a3a9c7f154634e788af1e16bf9e2e54d38d1237
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-04-05 15:10:19 +0530

    hurt

--- Assets/My_Assets/Scripts/Player/Healths.cs
+++ Assets/My_Assets/Scripts/Player/Healths.cs
@@ -5,51 +5,50 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-public class Healths : MonoBehaviour, IDamageable
+public class Healths : MonoBehaviour
 {
-    private Animator anime;
-    [SerializeField] private GameObject PlayerDestroy;
-    [field: SerializeField, Min(0)] public int max { set; private get; } = 100;
-    [SerializeField, ReadOnly] private int value;
-    [SerializeField, Range(0f, 5f)] private int damageMultipler = 1;
-    [SerializeField] private Slider HealthBar;
-    private float Delay = 0.3f;
+    [SerializeField] private int max = 100, min = 0;
+    [SerializeField] private int value;
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private int TakeDamage;
+      private Animator anime;
+    [SerializeField] private GameObject GameOver;
+
     private void Awake()
     {
         anime = GetComponent<Animator>();
-        PlayerDestroy = GetComponent<GameObject>();
     }
     public int Value() => value;
     private void Start()
     {
-        HealthBar.value = max;
+        healthBar.value = max;
+        value=max;
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemies"))
         {
-            HealthBar.value -= 25;
-          if(HealthBar.value < 0 )
+            value -= TakeDamage;
+            value = Mathf.Clamp(value , min, max);
+            healthBar.value = value;
+            if (value > 0)
             {
-                anime.SetBool("IsDead", true);
-                StartCoroutine(Destroy());
-                
+                anime.SetTrigger("IsHurt");
+            }
+            else
+            {
+                anime.Play("Death");
+                StartCoroutine(Wait(0.8f));
             }
         }
     }
-    void IDamageable.OnDamage(int amount)
+    IEnumerator Wait(float delay)
     {
-        value = Mathf.Clamp(value - amount * damageMultipler, 0, max);
-        if (value <= 0)
-        {
-
-        }
-    }
-    private IEnumerator Destroy()
-    {
-        yield return new WaitForSeconds(Delay);
-        Destroy(PlayerDestroy);
+        yield return new WaitForSeconds(delay);
+        GameOver.SetActive(true);
+        gameObject.SetActive(false);
     }
 }
 
